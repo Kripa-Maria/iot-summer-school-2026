d7addbed9f9ece5791721921174a011971f4a358
Kripa Maria <kripamariam2005@gmail.com>
2026-07-04 21:14:36 +0530

    docs: author comprehensive structural readme overview guide

--- week1/led_blink/led_blink.ino
+++ week1/led_blink/led_blink.ino
@@ -1,10 +1,22 @@
+/*
+ * Author: Kripa-Maria
+ * Date: July 2026
+ * Description: Potentiometer controlled variable LED blinking mechanism.
+ */
+
+int blinkCount = 0;
+
 void setup() {
   pinMode(13, OUTPUT);
+  Serial.begin(9600);
 }
-
 void loop() {
-  digitalWrite(13, HIGH);
-  delay(1000);
-  digitalWrite(13, LOW);
-  delay(1000);
+  int sensorVal = analogRead(A0);
+  
+  digitalWrite(13, HIGH); delay(sensorVal);
+  digitalWrite(13, LOW);  delay(sensorVal);
+  
+  blinkCount++;
+  Serial.print("Blink count: ");
+  Serial.println(blinkCount);
 }
