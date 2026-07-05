513367db1895728a5260655e701fb2e72fe6f802
jsswetha06-max <jsswetha06@gmail.com>
2026-07-05 20:10:56 +0530

    feat: add modified LED blink pattern

--- week1/blink_modified.ino
+++ week1/blink_modified.ino
@@ -1,19 +1,18 @@
-/*
- * Modified 
- * Added descriptive comments for readability.
- */
 void setup() {
   pinMode(13, OUTPUT);
 }
 
 void loop() {
-  // Blink the LED 3 times fast
-  for(int i = 0; i < 3; i++) {
+
+  // Blink LED 3 times quickly
+  for (int i = 0; i < 3; i++) {
     digitalWrite(13, HIGH);
     delay(200);
+
     digitalWrite(13, LOW);
     delay(200);
   }
-  // Pause cycle for exactly 2 seconds
+
+  // Pause for 2 seconds
   delay(2000);
 }
