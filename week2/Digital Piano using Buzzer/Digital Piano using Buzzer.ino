85febaff116aa655cf155549ec7b1d4ff7537bbe
Kripa-Maria <kripamariam2005@gmail.com>
2026-07-05 09:15:34 +0530

    feat: add multi-press dual chord substitute rule for Sol note (Q15)

--- week2/Digital Piano using Buzzer/Digital Piano using Buzzer.ino
+++ week2/Digital Piano using Buzzer/Digital Piano using Buzzer.ino
@@ -1,6 +1,7 @@
 const int KEY_PINS[] = {2, 3, 4, 5};
 const int BUZZER_PIN = 8;
 const int NOTE_FREQS[] = {262, 294, 330, 349};
+const int NOTE_SOL = 392;
 
 void setup() {
   for (int i = 0; i < 4; i++) {
@@ -10,15 +11,19 @@ void setup() {
 }
 
 void loop() {
-  bool anyPressed = false;
+  int pressCount = 0;
+  int lastPressedIndex = -1;
   for (int i = 0; i < 4; i++) {
     if (digitalRead(KEY_PINS[i]) == LOW) {
-      tone(BUZZER_PIN, NOTE_FREQS[i]);
-      anyPressed = true;
-      break;
+      pressCount++;
+      lastPressedIndex = i;
     }
   }
-  if (!anyPressed) {
+  if (pressCount > 1) {
+    tone(BUZZER_PIN, NOTE_SOL);
+  } else if (pressCount == 1) {
+    tone(BUZZER_PIN, NOTE_FREQS[lastPressedIndex]);
+  } else {
     noTone(BUZZER_PIN);
   }
 }
