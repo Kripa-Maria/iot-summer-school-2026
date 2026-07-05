244ac3479983284e89bc1f11a81eb79bd24873fc
Kripa-Maria <kripamariam2005@gmail.com>
2026-07-05 07:40:44 +0530

    feat: integrate potentiometer pin A0 to dynamically scale blink delay intervals (v1.2)

--- week1/Versioned LED Controller/Versioned LED Controller.ino
+++ week1/Versioned LED Controller/Versioned LED Controller.ino
@@ -6,10 +6,14 @@ void setup() {
 }
 
 void loop() {
+  int sensorValue = analogRead(A0);
+  // Map analog values (0-1023) to a comfortable delay range (100ms - 2000ms)
+  int blinkDelay = map(sensorValue, 0, 1023, 100, 2000);
+
   digitalWrite(13, HIGH);
-  delay(500);
+  delay(blinkDelay);
   digitalWrite(13, LOW);
-  delay(500);
+  delay(blinkDelay);
   
   blinkCount++;
   Serial.print("Blink count: ");
