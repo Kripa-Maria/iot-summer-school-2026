52dabf80a0afa798dec859cbeb4b148867fd1bd5
Kripa-Maria <kripamariam2005@gmail.com>
2026-07-05 09:15:32 +0530

    feat: implement basic 4-key digital piano with silence on release (Q15)

--- week2/Digital Piano using Buzzer/Digital Piano using Buzzer.ino
+++ week2/Digital Piano using Buzzer/Digital Piano using Buzzer.ino
@@ -1,47 +1,24 @@
 const int KEY_PINS[] = {2, 3, 4, 5};
-const int TOGGLE_PIN = 6;
 const int BUZZER_PIN = 8;
-
-const int MAJOR_FREQS[] = {262, 294, 330, 349}; 
-const int MINOR_FREQS[] = {262, 294, 311, 349}; 
-const int NOTE_SOL = 392;
-
-bool isMinorMode = false;
-bool lastToggleState = HIGH;
+const int NOTE_FREQS[] = {262, 294, 330, 349};
 
 void setup() {
   for (int i = 0; i < 4; i++) {
     pinMode(KEY_PINS[i], INPUT_PULLUP);
   }
-  pinMode(TOGGLE_PIN, INPUT_PULLUP);
   pinMode(BUZZER_PIN, OUTPUT);
 }
 
 void loop() {
-  bool currentToggleState = digitalRead(TOGGLE_PIN);
-  if (lastToggleState == HIGH && currentButtonState == LOW) {
-    delay(50);
-    isMinorMode = !isMinorMode;
-    while(digitalRead(TOGGLE_PIN) == LOW);
-  }
-  lastToggleState = currentToggleState;
-
-  int pressCount = 0;
-  int lastPressedIndex = -1;
-  
+  bool anyPressed = false;
   for (int i = 0; i < 4; i++) {
     if (digitalRead(KEY_PINS[i]) == LOW) {
-      pressCount++;
-      lastPressedIndex = i;
+      tone(BUZZER_PIN, NOTE_FREQS[i]);
+      anyPressed = true;
+      break;
     }
   }
-  
-  if (pressCount > 1) {
-    tone(BUZZER_PIN, NOTE_SOL);
-  } else if (pressCount == 1) {
-    int activeFreq = isMinorMode ? MINOR_FREQS[lastPressedIndex] : MAJOR_FREQS[lastPressedIndex];
-    tone(BUZZER_PIN, activeFreq);
-  } else {
+  if (!anyPressed) {
     noTone(BUZZER_PIN);
   }
 }
